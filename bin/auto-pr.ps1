807c0fd136ad2bf9abad9c1a81068d7c9ea2beb8
Dustin Venegas <DustinVenegas@users.noreply.github.com>
2022-09-05 22:40:28 -0700

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "DustinVenegas/scoop-extras:main"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
